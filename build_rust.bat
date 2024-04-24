118050f108ccd39d693b43fbc53bdd8a4293939c
LuzianU <52568586+LuzianU@users.noreply.github.com>
2024-04-24 12:28:25 +0200

    rebuild

--- build_rust.bat
+++ build_rust.bat
@@ -2,5 +2,5 @@ cd simple-audio-decoder-rs
 git pull
 cargo build --release
 cd ..
-xcopy /Y simple-audio-decoder-rs\target\release\simple_audio_decoder_rs.dll .\lib\
-xcopy /Y simple-audio-decoder-rs\src\simple_audio_decoder_rs.h .\lib\
+xcopy /Y simple-audio-decoder-rs\target\release\simple_audio_decoder_rs.dll .\
+xcopy /Y simple-audio-decoder-rs\src\simple_audio_decoder_rs.h .\
